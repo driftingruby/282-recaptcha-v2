2865f8986c5bf9d670ce432b742f30e84f6fa7b8
kobaltz <dave@k-innovations.net>
2021-02-07 23:43:29 -0500

    282

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   resources :users
   get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
